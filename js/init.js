895dae52e0a8b8b980aa7c1d31bd6af1f00127aa
jencol <jennice.colaco@gmail.com>
2016-12-30 00:53:15 -0500

    Added all the files and images

---
+++ js/init.js
@@ -0,0 +1,39 @@
+(function($){
+  $(document).ready(function(){
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    $('.modal-trigger').leanModal();
+    $('.collapsible').collapsible();
+    $('.modal').modal({
+      dismissible:true, // Modal can be dismissed by clicking outside of the modal
+      opacity:.5, // Opacity of modal background
+      in_duration:300, // Transition in duration
+      out_duration:200, // Transition out duration
+      starting_top:'4%', // Starting top style attribute
+      ending_top:'10%', // Ending top style attribute
+
+	 });
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+
+
+//BACKUP
+// (function($){
+//   $(function(){
+//     $('.button-collapse').sideNav();
+//     $('.parallax').parallax();
+//     $('.modal-trigger').leanModal();
+//     $('.carousel').carousel();
+//     $('.modal').modal({
+//       dismissible:true, // Modal can be dismissed by clicking outside of the modal
+//       opacity:.5, // Opacity of modal background
+//       in_duration:300, // Transition in duration
+//       out_duration:200, // Transition out duration
+//       starting_top:'4%', // Starting top style attribute
+//       ending_top:'10%', // Ending top style attribute
+//     }
+//   );
+
+//   }); // end of document ready
+// })(jQuery); // end of jQuery name space
